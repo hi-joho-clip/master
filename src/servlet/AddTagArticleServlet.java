ce754aa6ce08bde7a9c82231bb46de80ebdf2d90
126006 <126006@imac-14-24.mr.hi-joho.ac.jp>
2015-11-27 13:39:35 +0900

    記事のサーブレットを複数作成

--- src/servlet/AddTagArticleServlet.java
+++ src/servlet/AddTagArticleServlet.java
@@ -1,12 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import beansdomain.ArticleBean;
 
 /**
  * Servlet implementation class AddTagArticleServlet
@@ -41,6 +45,22 @@ public class AddTagArticleServlet extends HttpServlet {
 		}else if(セッション情報がないなら){
 			//ログイン画面に戻る
 		}*/
+		ArrayList<String> tag_body_list = new ArrayList<String>();
+		int user_id =0;//sessionからuser_idを取得
+		int article_id = Integer.parseInt(request.getParameter("article_id"));
+		tag_body_list = null; //クライアントからタグリストをもらう
+		try {
+			ArticleBean articlebean = new ArticleBean();
+			articlebean.setArticle_id(article_id);
+			if(articlebean.addArticleTag(user_id,tag_body_list)){
+				//成功したポップアップを表示
+			}else{
+				//失敗したポップアップを表示
+			}
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 	}
 
 }
