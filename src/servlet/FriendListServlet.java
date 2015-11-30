62386519d733f761b8dad11358c12173400e3ceb
121015 <121015@imac-14-16.mr.hi-joho.ac.jp>
2015-11-30 15:09:48 +0900

    フレンド機能に枠組みプラス

--- src/servlet/FriendListServlet.java
+++ src/servlet/FriendListServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -10,16 +11,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import net.arnx.jsonic.JSON;
+
 import beansdomain.Friend;
 
-@WebServlet("/FriendListServlet")
+@WebServlet("/friendlist")
 public class FriendListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     public FriendListServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
+
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		perform(request, response);
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -29,10 +36,19 @@ public class FriendListServlet extends HttpServlet {
 	private void perform(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		HttpSession session = request.getSession(true);
-		int own_user_id = Integer.parseInt(request.getParameter("own_user_id"));
-
 		Friend friendbeans = new Friend();
 		ArrayList<Friend> friend_list = new ArrayList<Friend>();
+
+
+		//セッション情報の名前は変更する必要あり
+		int own_user_id = own_user_id= 2;
+		//Integer.parseInt(request.getParameter("user_id"));
+
+
+		response.setContentType("application/json; charset=utf-8");
+		response.setHeader("Cache-Control", "private");
+		PrintWriter out = response.getWriter();
+
 
 		try {
 			friend_list = friendbeans.getFriendList(own_user_id);
@@ -40,9 +56,8 @@ public class FriendListServlet extends HttpServlet {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-		session.setAttribute("friendList", friend_list);
+		out.println(JSON.encode(friend_list, true).toString());
 		//フレンド一覧するページへ
-		request.getRequestDispatcher("/.jsp").forward(request, response);
 
 	}
 }
