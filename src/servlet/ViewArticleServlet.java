759a66c49f6e9c432ec85acf913d112273593bc2
126006 <126006@imac-14-24.mr.hi-joho.ac.jp>
2015-12-07 17:53:17 +0900

    記事についている複数のタグを記事表示のときに付与する処理を追加

--- src/servlet/ViewArticleServlet.java
+++ src/servlet/ViewArticleServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import net.arnx.jsonic.JSON;
+
+import beansdomain.TagBean;
 
 import beansdomain.ArticleBean;
 
@@ -30,8 +35,9 @@ public class ViewArticleServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		perform(request, response);
 	}
 
 	/**
@@ -39,6 +45,9 @@ public class ViewArticleServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		perform(request, response);
+	}
+	private void perform(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(true);
 		/*if(セッション情報があるなら){
 			//何もしない
@@ -49,19 +58,23 @@ public class ViewArticleServlet extends HttpServlet {
 		******ある記事を選択して表示した際******
 		****************************************/
 		//記事を表示
-		int article_id = Integer.parseInt(request.getParameter("article_id"));
+		int user_id =1;//sessionからuser_idを取得
+		//int article_id = Integer.parseInt(request.getParameter("article_id"));
+		int article_id = 1;
+		ArticleBean articlebean = new ArticleBean();
+		ArrayList<ArticleBean> article_list = new ArrayList<ArticleBean>();
+		TagBean tagbean = new TagBean();
+		ArrayList<TagBean> tag_list = new ArrayList<TagBean>();
 		try {
-
-			ArticleBean articlebean = new ArticleBean();
-			ArrayList<ArticleBean> article_list = new ArrayList<ArticleBean>();
 			articlebean.setArticle_id(article_id);
 			article_list=articlebean.viewArticle();
-			session.setAttribute("articlelist", article_list);
-			request.getRequestDispatcher("/").forward(request, response);//リンク先で値をGETして記事一覧表示
-
+			tag_list = tagbean.viewExistingTag(user_id, article_id);
 		} catch (Exception e) {
 			e.getStackTrace();
 		}
+		response.setContentType("application/json;charset=UTF-8");
+		response.setHeader("Cache-Control", "private");
+		PrintWriter out = response.getWriter();
+		out.println(JSON.encode(article_list, true).toString()+JSON.encode(tag_list, true).toString());
 	}
-
 }
