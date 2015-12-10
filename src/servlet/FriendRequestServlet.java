20189d1a8c7e00f3f7f7432efa4e59c5a894aa66
121015 <121015@imac-14-16.mr.hi-joho.ac.jp>
2015-12-10 17:02:46 +0900

    フレンドリスト ・・・フレンド承認・否認Servlet ・・・フレンド登録者一覧Servlet

--- src/servlet/FriendRequestServlet.java
+++ src/servlet/FriendRequestServlet.java
@@ -14,11 +14,11 @@ import net.arnx.jsonic.JSON;
 
 import beansdomain.Friend;
 
-@WebServlet("/friendlist")
-public class FriendListServlet extends HttpServlet {
+@WebServlet("/SearchFriendListServlet")
+public class FriendRequestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public FriendListServlet() {
+	public FriendRequestServlet() {
 		super();
 	}
 
@@ -34,7 +34,7 @@ public class FriendListServlet extends HttpServlet {
 		perform(request, response);
 	}
 
-	// フレンド登録一覧用
+	// フレンド承認・否認 状態コード:1 を取得
 	private void perform(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
@@ -54,17 +54,26 @@ public class FriendListServlet extends HttpServlet {
 			friend_list = friendbeans.getFriendList(own_user_id);
 
 			for (int i = 0; i < friend_list.size(); i++) {
-				if (friend_list.get(i).getStatus() != 1) {
+				if (friend_list.get(i).getStatus() == 1) {
 
 					request_friend_list.add(friend_list.get(i));
 
+					// setFriend_id(friend_list.get(i).getFriend_id());
+					// request_friend_list.get(i).setOwn_user_id(friend_list.get(i).getOwn_user_id());
+					// request_friend_list.get(i).setFriend_user_id(friend_list.get(i).getFriend_user_id());
+					// request_friend_list.get(i).setShare_id(friend_list.get(i).getShare_id());
+					// request_friend_list.get(i).setStatus(friend_list.get(i).getStatus());
+					// request_friend_list.get(i).setModified(friend_list.get(i).getModified());
+					// request_friend_list.get(i).setCreated(friend_list.get(i).getCreated());
+					// request_friend_list.get(i).setAcceptdate(friend_list.get(i).getAcceptdate());
 				}
 			}
+
 		} catch (Exception e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-		out.println(JSON.encode(friend_list, true).toString());
-
+		out.println(JSON.encode(request_friend_list, true).toString());
 	}
+
 }
