77896cdb4fe95bd37cc7e331f93f64ae7aa6cd57
126006 <126006@imac-14-24.mr.hi-joho.ac.jp>
2015-12-16 18:52:31 +0900

    タグリストをサーブレットで取得

--- WebContent/js/create_list.js
+++ WebContent/js/create_list.js
@@ -67,76 +67,103 @@ var item3 =	[ 	 "grid-item grid-item--3",
 		];
 
 var item = [item1, item2, item3];
-	var get_mylists = function(json) {
-		var random =Math.floor(Math.random()*3);
-		console.log(random);
-		// i=8, 876543210
 
-		$myList = $("<div class='grid-sizer'></div>");
-		$grid.prepend($myList).isotope('prepended', $myList).trigger('create');
-		for ( var i = json.length - 1; i >= 0; i--) {
+//タグを編集するときにArticle_idも送りたいので実装
+function getArticle_id(article_id){
+	var articleid = "<input type='hidden' value='"+article_id+"' name='article_id'>";
+	document.getElementById('article_id').innerHTML = articleid;
+}
 
-			$myList = $("<div class='"+item[random][i] + " mosaic-block bar'>" +
-							"<div class='mosaic-overlay'>"+
-								"<div id='menu'>"+
-									"<ol>"+
-										"<li><a href='"+json[i].url+"'>"+json[i].url+"</a></li>"+
-									"</ol>"+
-								"</div>"+
-								"<div id='menu2'>"+
-									"<a href='a'><img src='img/trash1.png' align='right'width='20'height='20'></img></a>"+
-									"<a href='i'><img src='img/share1.png' align='right'width='20'height='20'></img></a>"+
-									"<div class='remodal-bg'>"+
-									  "<a href='#'data-remodal-target='modal'onclick='return false;'><img src='img/tag1.png'align='right' width='20'height='20'></img></a>"+
-									"</div>"+
-									"<a href='e'><img src='img/star1.png' align='right'width='20'height='20'></img></a>"+
-								"</div>"+
+var get_mylists = function(json) {
+	var random =Math.floor(Math.random()*3);
+
+	$myList = $("<div class='grid-sizer'></div>");
+	$grid.prepend($myList).isotope('prepended', $myList).trigger('create');
+	for ( var i = json.length - 1; i >= 0; i--) {
+
+		$myList = $("<div class='"+item[random][i] + " mosaic-block bar'>" +
+						"<div class='mosaic-overlay'>"+
+							"<div id='menu'>"+
+								"<ol>"+
+									"<li><a href='"+json[i].url+"'>"+json[i].url+"</a></li>"+
+								"</ol>"+
 							"</div>"+
-							"<a href='../login/article.html?"+json[i].article_id+"'target='_blank'><div class='mosaic-backdrop relative'>" +
-								"<img src='http://www.kk1up.jp/wp-content/uploads/2015/07/201507290001-17.jpg'width='100%'height='100%'alt='"+json[i].title+"'/>" +
-								"<p class='absolute'>"+json[i].title+"</p>" +
-							"</div></a>"+
-						"</div>");
-			$grid.prepend($myList).isotope('prepended', $myList).trigger('create');
-		}
-		jQuery(function($){
-			$('.bar').mosaic({
-				animation	:	'slide'		//fade or slide
-			});
-	    });
-	};
-	var get_favlists = function(json) {
-		var random =Math.floor(Math.random()*3);
-		console.log(random);
-		// i=8, 876543210
-		$myList = $("<div class='grid-sizer'></div>");
-		$grid.prepend($myList).isotope('prepended', $myList).trigger('create');
-		for ( var i = json.length - 1; i >= 0; i--) {
-
-			$favList = $("<div class='"+item[random][i] + " mosaic-block bar'>" +
-							"<div class='mosaic-overlay'>"+
-								"<div id='menu'>"+
-									"<ol>"+
-										"<li><a href='"+json[i].url+"'>"+json[i].url+"</a></li>"+
-									"</ol>"+
+							"<div id='menu2'>"+
+								"<a href='a'><img src='img/trash1.png' align='right'width='20'height='20'></img></a>"+
+								"<a href='i'><img src='img/share1.png' align='right'width='20'height='20'></img></a>"+
+								"<div class='remodal-bg'>"+
+								 "<a href='#' data-remodal-target='modal' onclick='javascript:getTagArticle("+json[i].article_id+");return false;'>"+
+								  "<img src='img/tag1.png'align='right' width='20'height='20'></img>" +
+								  "</a>"+
 								"</div>"+
-								"<div id='menu2'>"+
-									"<a href='a'><img src='img/trash1.png' align='right'width='20'height='20'></img></a>"+
-									"<a href='i'><img src='img/share1.png' align='right'width='20'height='20'></img></a>"+
-									"<a href='u'><img src='img/tag1.png'align='right' width='20'height='20'></img></a>"+
-									"<a href='e'><img src='img/star1.png' align='right'width='20'height='20'></img></a>"+
-								"</div>"+
+								"<a href='e'><img src='img/star1.png' align='right'width='20'height='20'></img></a>"+
 							"</div>"+
-							"<a href='../login/article.html?"+json[i].article_id+"'target='_blank'><div class='mosaic-backdrop relative'>" +
-								"<img src='http://www.kk1up.jp/wp-content/uploads/2015/07/201507290001-17.jpg'width='100%'height='100%'alt='"+json[i].title+"'/>" +
-								"<p class='absolute'>"+json[i].title+"</p>" +
-							"</div></a>"+
-						"</div>");
-			$grid.prepend($favList).isotope('prepended', $favList).trigger('create');
-		}
-		jQuery(function($){
-			$('.bar').mosaic({
-				animation	:	'slide'		//fade or slide
-			});
-	    });
-	};
+						"</div>"+
+						"<a href='../login/article.html?"+json[i].article_id+"'target='_blank'><div class='mosaic-backdrop relative'>" +
+							"<img src='http://www.kk1up.jp/wp-content/uploads/2015/07/201507290001-17.jpg'width='100%'height='100%'alt='"+json[i].title+"'/>" +
+							"<p class='absolute'>"+json[i].title+"</p>" +
+						"</div></a>"+
+					"</div>");
+		$grid.prepend($myList).isotope('prepended', $myList).trigger('create');
+	}
+	jQuery(function($){
+		$('.bar').mosaic({
+			animation	:	'slide'		//fade or slide
+		});
+    });
+};
+//"<a href='#'data-remodal-target='modal'onclick='javascript:getArticle_id("+json[i].article_id+");return false;'>" ;
+var get_favlists = function(json) {
+	var random =Math.floor(Math.random()*3);
+	console.log(random);
+	// i=8, 876543210
+	$myList = $("<div class='grid-sizer'></div>");
+	$grid.prepend($myList).isotope('prepended', $myList).trigger('create');
+	for ( var i = json.length - 1; i >= 0; i--) {
+
+		$favList = $("<div class='"+item[random][i] + " mosaic-block bar'>" +
+						"<div class='mosaic-overlay'>"+
+							"<div id='menu'>"+
+								"<ol>"+
+									"<li><a href='"+json[i].url+"'>"+json[i].url+"</a></li>"+
+								"</ol>"+
+							"</div>"+
+							"<div id='menu2'>"+
+								"<a href='a'><img src='img/trash1.png' align='right'width='20'height='20'></img></a>"+
+								"<a href='i'><img src='img/share1.png' align='right'width='20'height='20'></img></a>"+
+								"<div class='remodal-bg'>"+
+								  "<a href='#'data-remodal-target='modal'onclick='javascript:getArticle_id("+json[i].article_id+");return false;'>" +
+								  "<img src='img/tag1.png'align='right' width='20'height='20'></img>" +
+								  "</a>"+
+								"</div>"+
+								"<a href='e'><img src='img/star1.png' align='right'width='20'height='20'></img></a>"+
+							"</div>"+
+						"</div>"+
+						"<a href='../login/article.html?"+json[i].article_id+"'target='_blank'><div class='mosaic-backdrop relative'>" +
+							"<img src='http://www.kk1up.jp/wp-content/uploads/2015/07/201507290001-17.jpg'width='100%'height='100%'alt='"+json[i].title+"'/>" +
+							"<p class='absolute'>"+json[i].title+"</p>" +
+						"</div></a>"+
+					"</div>");
+		$grid.prepend($favList).isotope('prepended', $favList).trigger('create');
+	}
+	jQuery(function($){
+		$('.bar').mosaic({
+			animation	:	'slide'		//fade or slide
+		});
+    });
+};
+var get_tag_article_lists = function(json){
+	for(var i=0;i<json.length;i++){
+		$('#tag-it').append('<li>' + json[i].tag_body +'</li>');
+	}
+	$(document).ready(function(){
+		$('#tag-it').tagit({fieldName:"tags[]"});
+	});
+
+
+	/*for(var i=0;i<json.length;i++){
+		$('#tag-it').tagit({ itemName:json[i].tag_body,fieldName: "tags[]"});
+	}	*/
+
+
+};
