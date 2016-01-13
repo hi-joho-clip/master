a33c6580059e64c95395037fc979a40bea68e971
121015 <121015@imac-14-16.mr.hi-joho.ac.jp>
2016-01-13 18:16:54 +0900

    ユーザ設定完成

--- WebContent/js/user_script.js
+++ WebContent/js/user_script.js
@@ -1,134 +1,131 @@
-
 //新規登録後
-function addUser(){
+function addUser() {
 
 	var jsonParam = null;// 送りたいデータ
 	var URL = "http://localhost:8080/clipMaster/adduser";
-	var userList="";
+	var userList = "";
 	var setappend = function(json) {
-		userList = json.ErrorMessage +"<br>";
-		document.getElementById('info').innerHTML=userList;
+		userList = json.ErrorMessage + "<br>";
+		document.getElementById('info').innerHTML = userList;
 	};
 	getJSON(URL, jsonParam, setappend);
 }
 
-//ユーザ削除
-function deleteUser(){
+// ユーザ削除
+function deleteUser() {
 
 	var jsonParam = null;// 送りたいデータ
 	var URL = "http://localhost:8080/clipMaster/deleteuser";
-	var userList="";
+	var userList = "";
 	var setappend = function(json) {
-		userList = "ニックネーム:" + json.nickname + "<br>" +"メールアドレス:"+json.mailaddress+"<br>";
-		document.getElementById('info').innerHTML=userList;
+		userList = "ニックネーム:" + json.nickname + "<br>" + "メールアドレス:"
+				+ json.mailaddress + "<br>";
+		document.getElementById('info').innerHTML = userList;
 	};
 	getJSON(URL, jsonParam, setappend);
 }
 
-
-//ユーザ情報の表示
-function getUserList(){
-
-
+// ユーザ情報の表示
+function getUserList() {
 
 	var URL = "http://localhost:8080/clipMaster/viewuser";
-	var userList="";
+	var userList = "";
 	var setappend = function(json) {
-		userList = "ニックネーム:" + json.nickname + "<br>" +"メールアドレス:"+json.mailaddress+"<br>";
-		document.getElementById('info').innerHTML=userList;
-		var kyoka="";
-		var kyohi="";
-		if(json.friend_flag==0){
-			kyoka="selected='selected'";
-			kyohi="";
-		}else{
-			kyoka="";
-			kyohi="select='selected'";
+		userList = "ニックネーム:" + json.nickname + "<br>" + "メールアドレス:"
+				+ json.mailaddress + "<br>";
+		document.getElementById('info').innerHTML = userList;
+		var kyoka = "";
+		var kyohi = "";
+		var flag="";
+		if (json.friend_flag == 0) {
+			kyoka = "checked";
+			kyohi = "";
+			flag="checked";
+		} else {
+			flag="";
+			kyoka = "";
+			kyohi = "checked";
 		}
-		var $onoffkun=$("<div class='ui-field-contain'>"+
-		"<label for='on-off-flip'>フレンド申請</label>"+
-		"<select name='on-off-flip' id='on-off-flip' data-role='flipswitch' onchange='test()'>"+
-		"<option value='off'"+kyohi+">拒否</option>"+
-		"<option value='on' "+kyoka+">許可</option>"+
-		"</select>"+
-		"</div>");
-		$("#onoff").append($onoffkun).trigger("create");
-
-
+		var $onoffkun = $("<input type='checkbox' name='onoffswitch' class='onoffswitch-checkbox' id='myonoffswitch' "+flag+">"
+				+ "<label class='onoffswitch-label' for='myonoffswitch' onclick='test()'>"
+				+ "<span  class='onoffswitch-inner'"+kyoka+"></span>"
+				+ "<span  class='onoffswitch-switch'"+kyohi+"></span>" + "</label>");
+		$("#onoffswitch").append($onoffkun).trigger("create");
 
 	};
 	getJSON(URL, null, setappend);
 }
 
-//ニックネーム変更画面
-function NickName(){
+// ニックネーム変更画面
+function NickName() {
 
 	var jsonParam = null;// 送りたいデータ
 	var URL = "http://localhost:8080/clipMaster/viewuser";
-	var userList="";
+	var userList = "";
 	var setappend = function(json) {
 		userList = "ニックネーム:" + json.nickname + "<br>";
-		document.getElementById('info').innerHTML=userList;
+		document.getElementById('info').innerHTML = userList;
 	};
 	getJSON(URL, jsonParam, setappend);
 }
 
-//メールアドレス変更画面
-function MailAddress(){
+// メールアドレス変更画面
+function MailAddress() {
 
 	var jsonParam = null;// 送りたいデータ
 	var URL = "http://localhost:8080/clipMaster/viewuser";
-	var userList="";
+	var userList = "";
 	var setappend = function(json) {
-		userList = "メールアドレス:"+ json.mailaddress + "<br>";
-		document.getElementById('info').innerHTML=userList;
+		userList = "メールアドレス:" + json.mailaddress + "<br>";
+		document.getElementById('info').innerHTML = userList;
 	};
 	getJSON(URL, jsonParam, setappend);
 }
 
-function MailAddress(){
+function MailAddress() {
 
 	var jsonParam = null;// 送りたいデータ
 	var URL = "http://localhost:8080/clipMaster/viewuser";
-	var userList="";
+	var userList = "";
 	var setappend = function(json) {
-		userList = "メールアドレス:"+ json.mailaddress + "<br>";
-		document.getElementById('info').innerHTML=userList;
+		userList = "メールアドレス:" + json.mailaddress + "<br>";
+		document.getElementById('info').innerHTML = userList;
 	};
 	getJSON(URL, jsonParam, setappend);
 }
 
-//フレンド申請  承認設定
-function AcceptFriend(){
+// フレンド申請 承認設定
+function AcceptFriend() {
 
 	var jsonParam = null;// 送りたいデータ
 	var URL = "http://localhost:8080/clipMaster/acceptfriend";
-	var userList="";
+	var userList = "";
 	var setappend = function(json) {
 		userList = json.Message;
-		document.getElementById('info').innerHTML=userList;
+		document.getElementById('info').innerHTML = userList;
 	};
 	getJSON(URL, jsonParam, setappend);
 }
 
-//フレンド申請  拒否設定
-function DenyFriend(){
+// フレンド申請 拒否設定
+function DenyFriend() {
 
 	var jsonParam = null;// 送りたいデータ
 	var URL = "http://localhost:8080/clipMaster/denyfriend";
-	var userList="";
+	var userList = "";
 	var setappend = function(json) {
 		userList = json.Message;
-		document.getElementById('info').innerHTML=userList;
+		document.getElementById('info').innerHTML = userList;
 	};
 	getJSON(URL, jsonParam, setappend);
 }
 
-function test(){
-	var data = document.getElementById('on-off-flip').value;
-	if(data == "on"){
+function test() {
+	var data = document.getElementById('myonoffswitch').checked;
+
+	if (data == false) {
 		AcceptFriend();
-	}else if(data == "off"){
+	} else if (data == true) {
 		DenyFriend();
 	}
 }
